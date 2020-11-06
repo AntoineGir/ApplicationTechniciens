73a6c6662466aa09d1bb66e21d509d8bc3fe707e
AntoineGir <70748002+AntoineGir@users.noreply.github.com>
2020-11-06 17:20:33 +0100

    Ajoutez des fichiers projet.

---
+++ ApplicationTechniciens/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace ApplicationTechniciens
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void BtnRefrech_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (DatePrise.Text == "")
+            {
+                LblDateEntrez.Content = "entrez une date!!!!!!!";
+            }
+            else
+            {
+                string dt = DatePrise.Text;
+                LblDateEntrez.Content = dt;
+
+            }
+
+
+        }
+
+        private void BtnAjouter_Click(object sender, RoutedEventArgs e)
+        {
+           // Window wnd = new Window();
+            //wnd.ShowDialog();
+
+            AjouterReservation wnd = new AjouterReservation();
+            wnd.Show();
+            
+
+        }
+
+        private void LvDates_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
